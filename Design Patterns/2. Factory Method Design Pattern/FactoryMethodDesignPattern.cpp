4b4f5d0b124fb256464f250ff68f58ff593c0adb
naveen5655 <naveenbadisa5655@gmail.com>
2024-09-28 17:19:46 +0530

    added Factory Method design pattern

---
+++ Design Patterns/2. Factory Method Design Pattern/FactoryMethodDesignPattern.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum class LogLevel {
+    DEBUG,
+    INFO,
+    ERROR
+};
+
+class ILogger {
+    public:
+        virtual void log(string msg) = 0;
+        virtual ~ILogger() {}
+};
+
+class DebugLogger : public ILogger {
+    public:
+        void log(string msg);
+};
+
+void DebugLogger::log(string msg) {
+    cout << "DEBUG : " << msg << "\n";
+}
+
+class InfoLogger : public ILogger {
+    public:
+        void log(string msg);
+};
+
+void InfoLogger::log(string msg) {
+    cout << "INFO : " << msg << "\n";
+}
+
+class ErrorLogger : public ILogger {
+    public:
+        void log(string msg);
+};
+
+void ErrorLogger::log(string msg) {
+    cout << "ERROR : " << msg << "\n";
+}
+
+class ILoggerFactory {
+    public:
+        virtual ILogger* createLogger() = 0;
+};
+
+class DebugLoggerFactory : public ILoggerFactory {
+    public:
+        ILogger* createLogger() {
+            return new DebugLogger();
+        }
+};
+
+class InfoLoggerFactory : public ILoggerFactory {
+    public:
+        ILogger* createLogger() {
+            return new InfoLogger();
+        }
+};
+
+class ErrorLoggerFactory : public ILoggerFactory {
+    public:
+        ILogger* createLogger() {
+            return new ErrorLogger();
+        }
+};
+
+int main() {
+    ILoggerFactory* loggerFactory1 = new InfoLoggerFactory;
+    ILogger* infoLogger = loggerFactory1->createLogger();
+
+    ILoggerFactory* loggerFactory2 = new ErrorLoggerFactory;
+    ILogger* errorLogger = loggerFactory2->createLogger();
+
+    ILoggerFactory* loggerFactory3 = new DebugLoggerFactory;
+    ILogger* debugLogger = loggerFactory3->createLogger();
+
+    debugLogger->log("This is a debug log msg");
+    infoLogger->log("This is an info log msg");
+    errorLogger->log("This is an error log msg");
+
+    delete debugLogger;
+    delete infoLogger;
+    delete errorLogger;
+
+    return 0;
+}
+
+
+
+/*
+
+Output:
+
+DEBUG : This is a debug log msg
+INFO : This is an info log msg
+ERROR : This is an error log msg
+
+*/
